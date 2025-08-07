e6b442778df2f92eedc17a5dcb408e7bc060335b
owenclaudia88-hue <owenclaudia88@gmail.com>
2025-08-07 15:00:35 +0300

    Add files via upload

---
+++ app/elgiganten/offerRedirects.ts
@@ -0,0 +1,23 @@
+// This file maps offer IDs to actual redirect URLs (used server-side only)
+export const offerRedirectMap: Record<string, string> = {
+  "iphone-16-pro-max": "https://www.elgiganten.se/product/iphone-16-pro-max",
+  "elg-001": "https://www.elgiganten.se/all-products-sale",
+  "elg-002": "https://www.elgiganten.se/super-sale",
+  "elg-003": "https://www.elgiganten.se/student-discount",
+  "elg-004": "https://www.elgiganten.se/free-shipping",
+  "elg-005": "https://www.elgiganten.se/tv-discount",
+  "elg-006": "https://www.elgiganten.se/home-appliances-sale",
+  "elg-007": "https://www.elgiganten.se/computers-discount",
+  "elg-008": "https://www.elgiganten.se/gaming-accessories-sale",
+  "elg-009": "https://www.elgiganten.se/extra-discount-clearance",
+  "elg-010": "https://www.elgiganten.se/smartphones-sale",
+  "elg-011": "https://www.elgiganten.se/free-installation-appliances",
+  "elg-012": "https://www.elgiganten.se/new-customer-discount",
+  "elg-exp-001": "https://www.elgiganten.se/expired/black-friday-tv",
+  "elg-exp-002": "https://www.elgiganten.se/expired/christmas-shipping",
+  "elg-exp-003": "https://www.elgiganten.se/expired/summer-ac-sale",
+  "elg-exp-004": "https://www.elgiganten.se/expired/cyber-monday-gaming",
+  "elg-exp-005": "https://www.elgiganten.se/expired/easter-kitchen-sale",
+  "elg-exp-006": "https://www.elgiganten.se/expired/newyear-audio-sale",
+  "elg-exp-007": "https://www.elgiganten.se/expired/midsummer-delivery",
+}
