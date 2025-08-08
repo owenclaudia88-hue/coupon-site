b8a283d34236f915ea577b78d1dd52ab2456b403
owenclaudia88-hue <owenclaudia88@gmail.com>
2025-08-08 11:47:43 +0300

    Update route.ts

--- app/api/get-offer-url/route.ts
+++ app/api/get-offer-url/route.ts
@@ -5,7 +5,7 @@ const offerUrls: { [key: string]: string } = {
   'elg-001': 'https://www.elgiganten.se/all-products-sale',
   'elg-002': 'https://www.elgiganten.se/super-sale',
   'elg-003': 'https://www.elgiganten.se/student-discount',
-  'free-shipping': 'https//:www.elgiganten.se/free-shipping',
+  'free-shipping': 'https://www.elgiganten.se/free-shipping',
   'elg-005': 'https://www.elgiganten.se/tv-discount',
   'elg-006': 'https://www.elgiganten.se/home-appliances-sale',
   'elg-007': 'https://www.elgiganten.se/computers-discount',
@@ -38,6 +38,7 @@ export async function POST(request: Request) {
     return NextResponse.json({ error: 'Invalid request' }, { status: 400 })
   }
 }
+
 
 
 
