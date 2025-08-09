61d39c5905c19ea8ec55555a19c3061e299294e4
owenclaudia88-hue <owenclaudia88@gmail.com>
2025-08-09 17:11:39 +0300

    Create CaptchaRedirectClient.tsx

---
+++ app/power/verify/CaptchaRedirectClient.tsx
@@ -0,0 +1,8 @@
+// app/power/verify/CaptchaRedirectClient.tsx
+"use client"
+
+import CaptchaRedirect from "../../components/CaptchaRedirect"
+
+export default function CaptchaRedirectClient() {
+  return <CaptchaRedirect store="power" />
+}
