4c8260ff3079780d334a9663c442f13891597303
owenclaudia88-hue <owenclaudia88@gmail.com>
2025-08-09 17:19:24 +0300

    Update offerRedirects.ts

--- app/power/offerRedirects.ts
+++ app/power/offerRedirects.ts
@@ -1,19 +1,14 @@
 // app/power/offerRedirects.ts
 export const offerRedirects: Record<string, string> = {
-  "power-001": "https://www.power.se/",
-  "power-002": "https://www.power.se/",
-  "power-003": "https://www.power.se/",
-  "power-004": "https://www.power.se/",
-  "power-005": "https://www.power.se/",
-  "power-006": "https://www.power.se/",
-  "power-007": "https://www.power.se/",
-  "power-008": "https://www.power.se/",
-  "power-009": "https://www.power.se/",
-  "power-010": "https://www.power.se/",
-  "power-011": "https://www.power.se/",
-  "power-012": "https://www.power.se/",
-
-  // expired examples
-  "power-exp-001": "https://www.power.se/",
-  "power-exp-002": "https://www.power.se/",
+  "pow-001": "https://www.power.se/kampanj/allt/",
+  "pow-002": "https://www.power.se/kampanj/gaming/",
+  "pow-003": "https://www.power.se/kampanj/tv-och-ljud/",
+  "pow-004": "https://www.power.se/kampanj/datorer/",
+  "pow-005": "https://www.power.se/kampanj/smarta-hem/",
+  "pow-006": "https://www.power.se/kampanj/vitvaror/",
+  "pow-007": "https://www.power.se/kampanj/mobiler/",
+  "pow-008": "https://www.power.se/kampanj/rea/",
+  // expired examples if you want
+  "pow-exp-001": "https://www.power.se/expired/black-friday/",
+  "pow-exp-002": "https://www.power.se/expired/cyber-monday/",
 }
