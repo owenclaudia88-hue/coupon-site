d8ac88807c0930f4f92629dd03fe77a441711d36
owenclaudia88-hue <owenclaudia88@gmail.com>
2025-08-10 15:01:49 +0300

    Add files via upload

---
+++ app/elgiganten/GoogleTag.tsx
@@ -0,0 +1,21 @@
+import Script from "next/script"
+
+export default function GoogleTag() {
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17459630072"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17459630072');
+        `}
+      </Script>
+    </>
+  )
+}
