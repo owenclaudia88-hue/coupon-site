ccad244af525dd99589881af85d2a26a207b3421
owenclaudia88-hue <owenclaudia88@gmail.com>
2025-08-20 06:32:42 +0300

    Update GoogleTag.tsx

--- app/elgiganten/GoogleTag.tsx
+++ app/elgiganten/GoogleTag.tsx
@@ -5,7 +5,7 @@ export default function GoogleTag() {
     <>
       {/* Google tag (gtag.js) */}
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=AW-17459630072"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-xxxxxxxxxxxxxxx"
         strategy="afterInteractive"
       />
       <Script id="gtag-init" strategy="afterInteractive">
@@ -13,9 +13,10 @@ export default function GoogleTag() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'AW-17459630072');
+          gtag('config', 'AW-xxxxxxxxxxxxxxxxxxxx');
         `}
       </Script>
     </>
   )
 }
+
